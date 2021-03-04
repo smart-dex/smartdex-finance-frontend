8b373a688b836cd6aa9d090110c38c24024a6375
viet <viet.ha@sotatek.com>
2021-03-04 18:10:03 +0700

    team detal card

--- src/views/Teams/components/TeamCard.tsx
+++ src/views/Teams/components/TeamCard.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Card, CardHeader, CardBody, CommunityIcon, Heading, PrizeIcon, Text } from '@pancakeswap-libs/uikit'
+import { Card, CardHeader, CardBody, CommunityIcon, Heading, PrizeIcon, Text, Flex } from '@pancakeswap-libs/uikit'
 import { Team } from 'config/constants/types'
 import useI18n from 'hooks/useI18n'
-import ComingSoon from 'views/Profile/components/ComingSoon'
+// import ComingSoon from 'views/Profile/components/ComingSoon'
 import StatBox from 'views/Profile/components/StatBox'
 
 interface TeamCardProps {
@@ -20,40 +20,37 @@ const Wrapper = styled.div`
 
 const Avatar = styled.img`
   border-radius: 50%;
-  height: 64px;
-  margin-top: -12px;
-  width: 64px;
+  min-height: 64px;
+  min-width: 64px;
   border: solid 2px white;
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    height: 128px;
-    margin-top: -24px;
-    width: 128px;
+  ${({ theme }) => theme.mediaQueries.lg} {
+    height: 120px;
+    // margin-top: -24px;
+    width: 120px;
   }
 `
 
 const AvatarWrap = styled.div`
-  margin-bottom: 8px;
-  text-align: center;
+  // margin-bottom: 8px;
+  justify-items: center;
+  margin: auto 0;
 `
 
 const StyledCard = styled(Card)`
   overflow: visible;
 `
 
-const StyledCardHeader = styled(CardHeader)<{ bg: string }>`
-  position: relative;
+const StyledCardHeader = styled(CardHeader) <{ bg: string }>`
   background: url(${({ bg }) => bg});
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
   border-radius: 32px 32px 0 0;
-  padding-top: 0;
-  text-align: center;
+  display: flex;
 `
 
 const TeamName = styled(Heading).attrs({ as: 'h2' })`
-  font-size: 16px;
+  font-size: 32px;
 `
 
 const StatRow = styled.div`
@@ -68,6 +65,9 @@ const StatRow = styled.div`
     margin-bottom: 32px;
   }
 `
+const StyedTextName = styled(Flex)`
+  flex-direction:  colum;
+`
 
 const TeamCard: React.FC<TeamCardProps> = ({ team }) => {
   const TranslateString = useI18n()
@@ -79,10 +79,12 @@ const TeamCard: React.FC<TeamCardProps> = ({ team }) => {
           <AvatarWrap>
             <Avatar src={`/images/teams/${team.images.md}`} alt="team avatar" />
           </AvatarWrap>
-          <TeamName color={team.textColor}>{team.name}</TeamName>
-          <Text as="p" color={team.textColor}>
-            {team.description}
-          </Text>
+          <StyedTextName ml='16px' flexDirection='column' justifyContent='center'>
+            <TeamName color={team.textColor}>{team.name}</TeamName>
+            <Text fontSize='14px' as="p" color={team.textColor}>
+              {team.description}
+            </Text>
+          </StyedTextName>
         </StyledCardHeader>
         <CardBody>
           <StatRow>
@@ -95,7 +97,7 @@ const TeamCard: React.FC<TeamCardProps> = ({ team }) => {
             />
           </StatRow>
           <Heading as="h3">{TranslateString(1044, 'Team Achievements')}</Heading>
-          <ComingSoon />
+          {/* <ComingSoon /> */}
         </CardBody>
       </StyledCard>
     </Wrapper>
