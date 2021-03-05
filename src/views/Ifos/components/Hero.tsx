6ebd9a8b798e5e4493547f85da9156afd2e355b0
Hoang Son <son.hoang@sotatek.com>
2021-03-05 09:52:09 +0700

    fix style

--- src/views/Ifos/components/Hero.tsx
+++ src/views/Ifos/components/Hero.tsx
@@ -4,19 +4,22 @@ import { Heading, Text } from '@pancakeswap-libs/uikit'
 import Container from 'components/layout/Container'
 import useI18n from 'hooks/useI18n'
 
-const Title = styled(Heading).attrs({ as: 'h1', size: 'xl' })`
-  color: ${({ theme }) => theme.colors.secondary};
+const Title = styled(Heading).attrs({ as: 'h1' })`
+  color: #ffffff;
   margin-bottom: 24px;
+  font-size: 36px;
+  line-height: 44px;
+  font-weight: 700;
 `
 
 const Blurb = styled(Text)`
   color: #ffffff;
-  font-size: 20px;
-  font-weight: 600;
+  font-size: 16px;
+  font-weight: 500;
 `
 
 const StyledHero = styled.div`
-  background-image: linear-gradient(180deg, #53dee9 0%, #1fc7d4 100%);
+  background-image: linear-gradient(91.67deg, #0085ff 5.33%, #7e86ff 104.39%);
   padding-bottom: 40px;
   padding-top: 40px;
   margin-bottom: 32px;
