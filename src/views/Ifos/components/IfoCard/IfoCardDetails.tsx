8154ddf67c9b53d4254d98e93f7583a4ebd8d0a5
Sotatek HaTo <ha.to@sotatek.com>
2021-03-30 16:15:17 +0700

    fix/update

--- src/views/Ifos/components/IfoCard/IfoCardDetails.tsx
+++ src/views/Ifos/components/IfoCard/IfoCardDetails.tsx
@@ -88,7 +88,7 @@ const IfoCardDetails: React.FC<IfoCardDetailsProps> = ({
           <Text>{raiseAmount}</Text>
         </Item>
         <Item>
-          <Display>{TranslateString(586, 'CAKE to burn (USD)')}</Display>
+          <Display>{TranslateString(586, 'SDC to burn (USD)')}</Display>
           <Text>{cakeToBurn}</Text>
         </Item>
         <Item>
