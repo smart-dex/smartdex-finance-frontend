1372ea69bd7b1ac793ef19525077264616861249
VanPham <van.pham@sotatek.com>
2021-03-30 16:04:37 +0700

    fix ban 273

--- src/views/Lottery/components/PastLotteryRoundViewer/PastRoundCard.tsx
+++ src/views/Lottery/components/PastLotteryRoundViewer/PastRoundCard.tsx
@@ -16,7 +16,11 @@ interface PastRoundCardProps {
 const  CardStyle = styled(Card)`
   border: 1px solid ${({ theme }) => (theme.isDark ? darkColors.borderColor : lightColors.borderColor)};
   box-shadow: 10px 10px 30px rgba(120, 118, 148, 0.07);
-  border-radius: 40px !important;
+  border-radius: 30px !important;
+  ${({ theme }) => theme.mediaQueries.nav} {
+    border-radius: 40px !important;
+  }
+
 `
 const PastRoundCard: React.FC<PastRoundCardProps> = ({ error, data }) => {
   return <CardStyle>{error.message ? <PastRoundCardError error={error} /> : <PastRoundCardDetails data={data} />}</CardStyle>
