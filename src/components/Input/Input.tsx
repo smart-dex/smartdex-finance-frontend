b782265872d7aa2edceffd924adc08ccad5f6998
Sotatek-LocNgo <loc.ngo@sotatek.com>
2021-03-31 15:17:58 +0700

    import react-text-mask

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { lightColors, darkColors } from 'style/Color'
 import styled from 'styled-components'
+import MaskedInput from 'react-text-mask'
+import createNumberMask from 'text-mask-addons/dist/createNumberMask'
+import PropTypes from 'prop-types'
 
 export interface InputProps {
   endAdornment?: React.ReactNode
@@ -10,14 +13,58 @@ export interface InputProps {
   value: string
 }
 
-const Input: React.FC<InputProps> = ({ endAdornment, onChange, placeholder, startAdornment, value }) => {
+const defaultMaskOptions = {
+  suffix: '',
+  includeThousandsSeparator: true,
+  thousandsSeparatorSymbol: ',',
+  allowDecimal: true,
+  decimalSymbol: '.',
+  decimalLimit: 6, // how many digits allowed after the decimal
+  integerLimit: 7, // limit length of integer numbers
+  allowNegative: false,
+  allowLeadingZeroes: false,
+}
+
+const Input: React.FC<InputProps> = ({ endAdornment, onChange, placeholder, startAdornment, value, maskOptions }) => {
+  const currencyMask = createNumberMask({
+    ...defaultMaskOptions,
+    ...maskOptions,
+  })
   return (
     <StyledInputWrapper>
       {!!startAdornment && startAdornment}
-      <StyledInput placeholder={placeholder} value={value} onChange={onChange} />
+      {/* <StyledInput placeholder={placeholder} value={value} onChange={onChange} /> */}
+      <StyledInput>
+        <MaskedInput
+          onChange={onChange}
+          placeholder={placeholder}
+          value={value}
+        />
+      </StyledInput>
       {!!endAdornment && endAdornment}
     </StyledInputWrapper>
   )
+}
+
+Input.defaultProps = {
+  inputMode: 'numeric',
+  maskOptions: {},
+}
+
+Input.propTypes = {
+  inputmode: PropTypes.string,
+  maskOptions: PropTypes.shape({
+    suffix: PropTypes.string,
+    includeThousandsSeparator: PropTypes.boolean,
+    thousandsSeparatorSymbol: PropTypes.string,
+    allowDecimal: PropTypes.boolean,
+    decimalSymbol: PropTypes.string,
+    decimalLimit: PropTypes.string,
+    requireDecimal: PropTypes.boolean,
+    allowNegative: PropTypes.boolean,
+    allowLeadingZeroes: PropTypes.boolean,
+    integerLimit: PropTypes.number,
+  }),
 }
 
 const StyledInputWrapper = styled.div`
@@ -29,19 +76,22 @@ const StyledInputWrapper = styled.div`
   padding: 0 ${(props) => props.theme.spacing[3]}px;
 `
 
-const StyledInput = styled.input`
+const StyledInput = styled.div`
   width: 100%;
-  background: none;
-  border: 0;
-  color: ${({ theme }) => (theme.isDark ? darkColors.colorInput : lightColors.colorInput)};
-  flex: 1;
-  height: 56px;
-  margin: 0;
-  padding: 0;
-  outline: none;
-  font-weight: 600;
-  font-size: 14px;
-  line-height: 17px;
+  input {
+    width: 100%;
+    background: none;
+    border: 0;
+    color: ${({ theme }) => (theme.isDark ? darkColors.colorInput : lightColors.colorInput)};
+    flex: 1;
+    height: 56px;
+    margin: 0;
+    padding: 0;
+    outline: none;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 17px;
+  }
 `
 
 export default Input
