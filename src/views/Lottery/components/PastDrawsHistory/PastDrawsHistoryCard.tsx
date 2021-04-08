5e49358719e5e381074e71f403520ea85301720c
VanPham <van.pham@sotatek.com>
2021-04-08 10:57:43 +0700

    fix pan 254

--- src/views/Lottery/components/PastDrawsHistory/PastDrawsHistoryCard.tsx
+++ src/views/Lottery/components/PastDrawsHistory/PastDrawsHistoryCard.tsx
@@ -6,10 +6,7 @@ import { darkColors, lightColors } from 'style/Color'
 import HistoryChart from './HistoryChart'
 import Legend from './Legend'
 
-const HeadingStyle = styled(Heading)`
-  color: ${({ theme }) => (theme.isDark ? darkColors.text : lightColors.textMenuLeft)};
-  font-size: 18px;
-`
+
 const CardStyle = styled(Card)`
   background: ${({ theme }) => (theme.isDark ? darkColors.backIfo : lightColors.backIfo)};
   box-shadow: 10px 10px 30px rgba(120, 118, 148, 0.07);
@@ -17,12 +14,11 @@ const CardStyle = styled(Card)`
   border-radius: 40px;
 `
 const PastDrawsHistoryCard: React.FC = () => {
-  const TranslateString = useI18n()
+  
 
   return (
     <CardStyle>
       <CardBody>
-        <HeadingStyle >{TranslateString(746, 'History')}</HeadingStyle>
         <Legend />
         <HistoryChart />
       </CardBody>
