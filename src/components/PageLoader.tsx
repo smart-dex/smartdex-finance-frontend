5ca46fe433db4e269ef52f924e40b5cab1765134
VanPham <van.pham@sotatek.com>
2021-04-12 10:45:56 +0700

    fix pan 443

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
-import styled from 'styled-components'
-import { Spinner } from 'uikit-sotatek'
+import styled, { keyframes } from 'styled-components'
+import { Image } from 'uikit-sotatek'
 import Page from './layout/Page'
+
+const pulse = keyframes`
+  0% { transform: scale(1); }
+  60% { transform: scale(1.1); }
+  100% { transform: scale(1); }
+`
 
 const Wrapper = styled(Page)`
   display: flex;
   justify-content: center;
   align-items: center;
+  animation: ${pulse} 800ms linear infinite;
+  & > * {
+    width: 72px;
+  }
 `
 
 const PageLoader: React.FC = () => {
   return (
     <Wrapper>
-      <Spinner />
+      <Image src="/images/project.svg" width={100} height={100} alt="Your project here" />
     </Wrapper>
   )
 }
