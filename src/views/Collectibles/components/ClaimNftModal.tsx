7053e7e7216f8104510e83e1c48c884060dbaee6
VanPham <van.pham@sotatek.com>
2021-04-12 16:48:51 +0700

    fix pan 465

--- src/views/Collectibles/components/ClaimNftModal.tsx
+++ src/views/Collectibles/components/ClaimNftModal.tsx
@@ -6,6 +6,7 @@ import { useToast } from 'state/hooks'
 import { Button, InjectedModalProps, Modal, Text, Flex } from 'uikit-sotatek'
 import { Nft } from 'config/constants/types'
 import useI18n from 'hooks/useI18n'
+import { darkColors, lightColors, baseColors} from 'style/Color'
 
 interface ClaimNftModalProps extends InjectedModalProps {
   nft: Nft
@@ -20,6 +21,45 @@ const Actions = styled.div`
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 8px;
 `
+
+const TextStyle = styled(Text)`
+  color: ${({ theme }) => (theme.isDark ? darkColors.text : lightColors.textMenuLeft)};
+`
+
+const handleBgDarkMode = (theme)=>(
+  theme.isDark ? darkColors.buttonView : lightColors.buttonView
+)
+
+const handleColorDarkMode = (theme)=>(
+  theme.isDark ? 'rgba(255, 255, 255, 0.6)' : '#8F8FA0'
+)
+
+const ButtonConfirm = styled(Button)`
+  font-size: 12px;
+  padding: 0 12px;
+  height: 45px;
+  background: ${({ disabled, theme }) => (disabled ? handleBgDarkMode(theme) : baseColors.primary)} !important;
+  color: ${({ disabled, theme }) => (disabled ? handleColorDarkMode(theme) : lightColors.invertedContrast)} !important;
+  ${({ theme }) => theme.mediaQueries.nav} {
+    font-size: 16px;
+    padding: 0 24px;
+    height: 56px;
+  }
+`
+
+const ButtonCancel = styled(Button)`
+  color: ${ baseColors.primary};
+  border-color: ${ baseColors.primary};
+  font-size: 12px;
+  padding: 0 12px;
+  height: 45px;
+  ${({ theme }) => theme.mediaQueries.nav} {
+    font-size: 16px;
+    padding: 0 24px;
+    height: 56px;
+  }
+`
+
 const ClaimNftModal: React.FC<ClaimNftModalProps> = ({ nft, onSuccess, onDismiss }) => {
   const [isConfirming, setIsConfirming] = useState(false)
   const TranslateString = useI18n()
@@ -50,17 +90,17 @@ const ClaimNftModal: React.FC<ClaimNftModalProps> = ({ nft, onSuccess, onDismiss
     <Modal title={TranslateString(999, 'Claim Collectible')} onDismiss={onDismiss}>
       <ModalContent>
         <Flex alignItems="center" mb="8px" justifyContent="space-between">
-          <Text>{TranslateString(626, 'You will receive')}:</Text>
-          <Text bold>{`1x "${nft.name}" Collectible`}</Text>
+          <TextStyle>{TranslateString(626, 'You will receive')}:</TextStyle>
+          <TextStyle bold>{`1x "${nft.name}" Collectible`}</TextStyle>
         </Flex>
       </ModalContent>
       <Actions>
-        <Button variant="secondary" onClick={onDismiss}>
+        <ButtonCancel variant="secondary" onClick={onDismiss}>
           {TranslateString(462, 'Cancel')}
-        </Button>
-        <Button onClick={handleConfirm} disabled={!account || isConfirming}>
+        </ButtonCancel>
+        <ButtonConfirm onClick={handleConfirm} disabled={!account || isConfirming}>
           {TranslateString(464, 'Confirm')}
-        </Button>
+        </ButtonConfirm>
       </Actions>
     </Modal>
   )
