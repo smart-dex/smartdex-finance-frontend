b5fce65e0ed96efd12555a40f84ae6085945b23b
Sotatek-AnhDao <anh.dao@sotatek.com>
2021-04-16 16:30:05 +0700

    update ifo contract

--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -3,7 +3,7 @@ import { Ifo } from './types'
 const ifos: Ifo[] = [
   {
     id: 'arsenal',
-    address: '0xb804e65e6Edc99017A66c2395d3dBAcF9B26f3A9',
+    address: '0xcBdcFD15bcd44210E44d30F35271aF8490EAE087',
     isActive: true,
     name: 'Arsenal',
     subTitle: 'Token for Arsenal club',
@@ -18,7 +18,7 @@ const ifos: Ifo[] = [
     currency: 'SDC-BNB LP',
     currencyAddress: '0x1accbdfc32c26935ecb5768d58c195cd238c3932',
     tokenDecimals: 18,
-    releaseBlockNumber: 8017973,
+    releaseBlockNumber: 8025057,
     campaignId: '2',
   },
   // {
